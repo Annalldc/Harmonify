1a789d0abb0bfed913e8b0c730d6b585431b6a0a
feh3211 <felipenepomuceno2018@gmail.com>
2023-12-04 20:02:58 -0300

    mudando esqueceu a senha js

--- assets/JS/esqueceuSenha.js
+++ assets/JS/esqueceuSenha.js
@@ -1,11 +1,12 @@
-function enviarEmail() {
-    var email = document.getElementById('email').value;
-    var mensagemElemento = document.getElementById('mensagem');
-    var emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+// esqueceuSenha.js
+function realizarRecuperacaoSenha() {
+  var email = document.getElementById('email').value;
+  var mensagemElemento = document.getElementById('mensagem');
+  var emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
-    if (emailValido) {
-      mensagemElemento.innerHTML = 'Um e-mail de redefinição foi enviado para ' + email;
-    } else {
-      mensagemElemento.innerHTML = 'POR FAVOR, INFORME UM ENDEREÇO DE E-MAIL VÁLIDO.';
-    }
+  if (emailValido) {
+    mensagemElemento.innerHTML = 'Um e-mail de redefinição foi enviado para ' + email;
+  } else {
+    mensagemElemento.innerHTML = 'POR FAVOR, INFORME UM ENDEREÇO DE E-MAIL VÁLIDO.';
   }
+}
